6d3520ab59fb82b788d89106744e2cee40d3689d
muffin9 <jinha6707@gmail.com>
2024-10-23 19:11:33 +0900

    Next.js setting

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    domains: ["genius.com", "images.genius.com", "images.rapgenius.com"],
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
 };
 
 export default nextConfig;
